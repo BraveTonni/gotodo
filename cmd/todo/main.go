27533a7c68c5cbfd2578494fd2ecccc14398f958
bravetonni <bravetonni@gmail.com>
2026-09-23 23:16:27 +0300

    main server implement

--- cmd/todo/main.go
+++ cmd/todo/main.go
@@ -1,7 +1,51 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
+
+	core_logger "github.com/BraveTonni/gotodo/internal/core/logger"
+	core_http_server "github.com/BraveTonni/gotodo/internal/core/transport/http/server"
+	users_transport_http "github.com/BraveTonni/gotodo/internal/features/users/transport/http"
+	"go.uber.org/zap"
+)
 
 func main() {
-	fmt.Println("Hello, this is todo.")
+	ctx, cancel := signal.NotifyContext(
+		context.Background(),
+		syscall.SIGINT, syscall.SIGTERM,
+	)
+	defer cancel()
+
+	logger, err := core_logger.NewLogger(core_logger.NewConfigMust())
+
+	if err != nil {
+		fmt.Println("failed to init app logger: %w", err)
+		os.Exit(1)
+	}
+	defer logger.Close()
+
+	logger.Debug("Starting app")
+
+	usersTransportHttp := users_transport_http.NewUsersHTTPHandler(nil)
+
+	usersRoutes := usersTransportHttp.Routes()
+	apiVersionRouter := core_http_server.NewAPIVersionRouter(core_http_server.APIVersion1)
+
+	apiVersionRouter.RegisterRoutes(usersRoutes...)
+
+	httpServer := core_http_server.NewHTTPServer(
+		core_http_server.NewConfigMust(),
+		logger,
+	)
+
+	httpServer.RegisterAPIRouters(apiVersionRouter)
+
+	if err := httpServer.Run(ctx); err != nil {
+		logger.Error("http server run error", zap.Error(err))
+	}
+
 }
--- internal/features/users/transport/http/transport.go
+++ internal/features/users/transport/http/transport.go
@@ -1,5 +1,11 @@
 package users_transport_http
 
+import (
+	"net/http"
+
+	core_http_server "github.com/BraveTonni/gotodo/internal/core/transport/http/server"
+)
+
 type UsersHTTPHandler struct {
 	usersService UsersService
 }
@@ -12,5 +18,15 @@ func NewUsersHTTPHandler(
 ) *UsersHTTPHandler {
 	return &UsersHTTPHandler{
 		usersService: usersService,
+	}
+}
+
+func (h *UsersHTTPHandler) Routes() []core_http_server.Route {
+	return []core_http_server.Route{
+		{
+			Method:  http.MethodPost,
+			Path:    "/users",
+			Handler: h.CreateUser,
+		},
 	}
 }
