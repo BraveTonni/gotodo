08e1af1b6843351b5e91e8be4c9b88dc3db6d1c0
BraveTonni <dev@milksmm.local>
2026-09-24 11:46:38 +0300

    create user service

--- internal/core/domain/user.go
+++ internal/core/domain/user.go
@@ -1,5 +1,12 @@
 package domain
 
+import (
+	"fmt"
+	"regexp"
+
+	core_errors "github.com/BraveTonni/gotodo/internal/core/errors"
+)
+
 type User struct {
 	ID      int
 	Version int
@@ -32,4 +39,36 @@ func NewUserUninitialized(
 		fullName,
 		phoneNumber,
 	)
+}
+
+func (u *User) Validate() error {
+	fullNameLength := len([]rune(u.FullName))
+	if fullNameLength < 3 || fullNameLength > 100 {
+		return fmt.Errorf(
+			"invalid `fullname` length: %d: %w",
+			fullNameLength, core_errors.InvalidArgument,
+		)
+	}
+
+	if u.PhoneNumber != nil {
+		phoneNumberLen := len([]rune(*u.PhoneNumber))
+
+		if phoneNumberLen < 10 || phoneNumberLen > 15 {
+			return fmt.Errorf(
+				"invalid `phonenumber` length: %d: %w",
+				phoneNumberLen, core_errors.InvalidArgument,
+			)
+		}
+
+		re := regexp.MustCompile(`^+[0-9]+$`)
+
+		if !re.MatchString(*u.PhoneNumber) {
+			return fmt.Errorf(
+				"invalid `phonenumber` format: %w",
+				core_errors.InvalidArgument,
+			)
+		}
+	}
+
+	return nil
 }
---
+++ internal/features/users/service/service.go
@@ -0,0 +1,24 @@
+package users_service
+
+import (
+	"context"
+
+	"github.com/BraveTonni/gotodo/internal/core/domain"
+)
+
+type UsersService struct {
+	usersRepository UsersRepository
+}
+
+type UsersRepository interface {
+	CreateUser(
+		ctx context.Context,
+		user domain.User,
+	) (domain.User, error)
+}
+
+func NewUsersService(
+	usersRepository UsersRepository,
+) *UsersService {
+	return &UsersService{usersRepository}
+}
